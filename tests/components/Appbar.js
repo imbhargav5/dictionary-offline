917fc4f5d7a14cea8e41092a4987c67a7305d247
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2015-12-30 00:11:54 +0530

    Tests

---
+++ tests/components/Appbar.js
@@ -0,0 +1,38 @@
+import 'babel-polyfill';
+import React from 'react';
+// Create a fake global `window` and `document` object:
+var jsdom = require('mocha-jsdom');
+
+// Replace BigComplicatedComponent.js with a stub component.
+global.reactModulesToStub = [
+  'BigComplicatedComponent.js'
+];
+
+var assert = require('assert');
+
+describe('Appbar', function() {
+  jsdom();
+
+  it('is an element', function() {
+    var TestUtils = require('react-addons-test-utils');
+    var Appbar = require('../../lib/components/Appbar');
+
+    assert.equal(true,TestUtils.isElementOfType(<Appbar/>,Appbar));
+
+    // // Render a checkbox with label in the document
+    // var checkbox = TestUtils.renderIntoDocument(
+    //   <CheckboxWithLabel labelOn="On" labelOff="Off" />
+    // );
+
+    // // Verify that it's Off by default
+    // var label = TestUtils.findRenderedDOMComponentWithTag(
+    //   checkbox, 'label');
+    // assert.equal(label.getDOMNode().textContent, 'Off');
+
+    // // Simulate a click and verify that it is now On
+    // var input = TestUtils.findRenderedDOMComponentWithTag(
+    //   checkbox, 'input');
+    // TestUtils.Simulate.change(input);
+    // assert.equal(label.getDOMNode().textContent, 'On');
+  });
+});
