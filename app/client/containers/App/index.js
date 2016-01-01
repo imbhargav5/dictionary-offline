57d89e4f8793f5e92acad68faafa40a0277877eb
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2016-01-01 19:20:16 +0530

    fix/remove react-router

--- app/client/containers/App/index.js
+++ app/client/containers/App/index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Link} from 'react-router';
 import {fetchDefinition, fetchAllWords} from '../../actions/search';
 import Appbar from '../../components/Appbar';
 import AppBody from '../../components/Body';
