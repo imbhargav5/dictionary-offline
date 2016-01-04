2aeedf73f6cd2cb1e247bc63b451d21bd46e7ea6
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2016-01-04 11:38:48 +0530

    Added Footer

--- app/client/components/Footer/index.js
+++ app/client/components/Footer/index.js
@@ -13,8 +13,8 @@ class Footer extends React.Component{
 	render(){
 		return <div className={styles.root}>
 			<ul className={styles.social}>
-					<li className={styles.github}><a target="_blank" href="http://github.com/bhargav175/dictionary-offline">View Source on Github</a></li>
-					<li className={styles.github}><a target="_blank" href="http://github.com/bhargav175/dictionary-offline">MIT &copy; Bhargav Ponnapalli</a></li>
+					<li className={styles.github}><a target="_blank" href="https://github.com/bhargav175/dictionary-offline">View Source on Github</a></li>
+					<li className={styles.github}><a target="_blank" href="https://twitter.com/imbhargav5/">MIT &copy; Bhargav Ponnapalli</a></li>
 				</ul></div>
 	}
 
